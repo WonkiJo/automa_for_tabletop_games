b7b4b23a4aa9f74900c1f497705ca2961d7d535b
Wonki Jo <joweonki@gmail.com>
2024-07-12 21:49:34 +0900

    Update config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/REPO_NAME/' // REPO_NAME을 저장소 이름으로 변경
+    ? '/automa_for_tabletop_games/' // REPO_NAME을 저장소 이름으로 변경
     : '/'
 })
