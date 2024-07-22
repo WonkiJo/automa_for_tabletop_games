c7f44804eea3bd9ab2ba7fe6f682282d34fe8215
Wonki Jo <joweonki@gmail.com>
2024-07-22 21:56:12 +0900

    Apply webpack

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/automa_for_tabletop_games/' // REPO_NAME을 저장소 이름으로 변경
-    : '/'
-})
+    : '/',
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          compress: true,
+          mangle: true,
+          output: {
+            comments: false,
+          },
+        },
+      })],
+    },
+  },
+  productionSourceMap: false, // 프로덕션 빌드 시 소스 맵 생성 안 함
+});
